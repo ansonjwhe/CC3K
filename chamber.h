c15c6e86d572335d705ed9a154bff891af677c6d
gavinsuns <68662120+gavinsuns@users.noreply.github.com>
2021-12-10 18:08:15 -0500

    Implemented random empty cell generation inside given chamber.

--- chamber.h
+++ chamber.h
@@ -1,12 +1,12 @@
 #ifndef CHAMBER_H
 #define CHAMBER_H
 #include "cell.h"
+#include <vector>
+#include <memory>
 
 class Chamber{
-
+    std::vector<std::shared_ptr<Cell>> cells;
+    std::shared_ptr<Cell> getRandomEmptyCell(int chamber);
 };
-
-Cell getRandomEmptyCell():
-
 
 #endif
